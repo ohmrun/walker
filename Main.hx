51d3da39d8b1f43595eac20cfa20b3b6376b8d1e
0b1kn00b <polysemantic@gmail.com>
2021-06-29 12:03:35 +0200

    Removee from test rig

--- Main.hx
+++ Main.hx
@@ -1,5 +1,19 @@
 using eu.ohmrun.Walker;
 
+using stx.Nano;
+using stx.Pico;
+using stx.Log;
+
+#if js
+import js.Browser.*;
+#end
+
+
+import tink.core.Noise;
+import coconut.Ui.hxx;
+import eu.ohmrun.walker.html5.History;
+import eu.ohmrun.walker.test.HistoryTest;
+
 class Main {
 	static function main() {
 		trace("main");
@@ -16,6 +30,15 @@ class Main {
 				// 			//_.tag('x')		
 				// 	);
 					
-		eu.ohmrun.walker.Test.main();
+		//eu.ohmrun.walker.Test.main();
+		if(document.getElementById('app') == null){
+      __.log().info("mount");
+      final document            = js.Browser.document;
+      final el                  = document.createDivElement();
+            el.setAttribute("id",'app');
+            document.body.appendChild(el);
+      final value               = hxx('<HistoryTestRootView history=${new History({state : Noise})}/>');
+            coconut.ui.Renderer.mount(el,value);
+		}
 	}
 }
